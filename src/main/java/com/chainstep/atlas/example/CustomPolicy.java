3927b1855212fa15b20649ef1945fa47d7a237c4
Philipp Schorr <philipp.schorr@htwsaar.de>
2024-09-23 14:27:43 +0200

    removed some logging and else statements

--- src/main/java/com/chainstep/atlas/example/CustomPolicy.java
+++ src/main/java/com/chainstep/atlas/example/CustomPolicy.java
@@ -6,9 +6,8 @@ import id.walt.credentials.w3c.VerifiableCredential;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper; 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-
 
 @Slf4j
 public class CustomPolicy extends SimpleVerificationPolicy {
@@ -16,7 +15,8 @@ public class CustomPolicy extends SimpleVerificationPolicy {
     public CustomPolicy() {
     }
 
-    //TODO: Please change these Values to define which role of which company is allowed to access yor service.
+    // TODO: Please change these Values to define which role of which company is
+    // allowed to access yor service.
     private String legalRegistrationNumString = "DummyLRN";
     private String roleToGrantAccessString = "DummyRole";
 
@@ -29,12 +29,12 @@ public class CustomPolicy extends SimpleVerificationPolicy {
     // Returns false if this policy does not apply to Verifiable Credentials (VCs)
     public boolean getApplyToVC() {
         return false;
-     }
-  
+    }
+
     // Returns true if this policy applies to Verifiable Presentations (VPs)
     public boolean getApplyToVP() {
         return true;
-     }
+    }
 
     // The main verification method, checks the VC against custom policy
     @NotNull
@@ -46,101 +46,76 @@ public class CustomPolicy extends SimpleVerificationPolicy {
             Object object = vc.getProperties().get("verifiableCredential");
             ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
             String jsonString = ow.writeValueAsString(object);
-            //log.info("returning JSON String: " + jsonString);                      
+            // log.info("returning JSON String: " + jsonString);
 
             ObjectMapper objectMapper = new ObjectMapper();
-            
+
             // Parse the JSON string into an array of JsonNodes
             JsonNode rootNode = objectMapper.readTree(jsonString);
-            
+
             // Access the first element in the array (the credential itself)
             JsonNode credentialNode = rootNode.get(0);
 
-            // Check if the credential is signed by AtlasXAuth
-            if(isSignedByAtlasXAuth(credentialNode)){
-                log.info("isSignedByAtlasXAuth true");   
-            }
-            else{
-                log.info("isSignedByAtlasXAuth false! NO access!");
-                return VerificationPolicyResult.Companion.failure();  // Fail if not signed by AtlasXAuth
-            }
-
             // Check if the credential has the required legal registration number
-            if(isFromLegalRegistrationNumber(credentialNode, legalRegistrationNumString)){
-                log.info("isFromLegalRegistrationNumber true");   
+            if (isFromLegalRegistrationNumber(credentialNode, legalRegistrationNumString)) {
+                //log.info("isFromLegalRegistrationNumber true");
             }
-            else{
-                log.info("isFromLegalRegistrationNumber false! NO access!");
-                return VerificationPolicyResult.Companion.failure();  // Fail if the legal registration number does not match
+            if (!isFromLegalRegistrationNumber(credentialNode, legalRegistrationNumString)) {
+                log.info("isFromLegalRegistrationNumber: Given LRN has no access!");
+                return VerificationPolicyResult.Companion.failure(); // Fail if the legal registration number does not
+                                                                     // match
             }
 
             // Check if the credential has the required role
-            if(isRole(credentialNode, roleToGrantAccessString)){
-                log.info("isRole true");   
+            if (isRole(credentialNode, roleToGrantAccessString)) {
+                //log.info("isRole true");
             }
-            else{
-                log.info("isRole false! NO access!");
-                return VerificationPolicyResult.Companion.failure();  // Fail if the role is not "remoteControl"
+            if (!isRole(credentialNode, roleToGrantAccessString)) {
+                log.info("isRole: Given role has no access!");
+                return VerificationPolicyResult.Companion.failure(); // Fail if the role is not "remoteControl"
             }
 
             // If all checks pass, return success
+            log.info("Access to the service is granted.");
             return VerificationPolicyResult.Companion.success();
-            
+
         } catch (Exception e) {
-            log.info("error during parsing credential subject" + e);
+            log.info("Error during parsing the credential subject" + e);
             return VerificationPolicyResult.Companion.failure(new Exception("parsing Error in the CustomPolicy"));
         }
 
     }
 
-        private boolean isSignedByAtlasXAuth(JsonNode jsonNode ) {
-            // get to the legalRegistrationNumber field
-            JsonNode proofCreator = jsonNode.get("proof").get("verificationMethod");
-            log.info("proofCreator  : " + proofCreator.toString());
-            log.info("issuer  : " + jsonNode.get("issuer").asText());
-            
-            if (proofCreator.toString().contains("did:web:authority.atlas.cartrust.com:gx:did")){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-
-    // Method to check if the credential's legal registration number matches the expected one
+    // Method to check if the credential's legal registration number matches the
+    // expected one
     private boolean isFromLegalRegistrationNumber(JsonNode jsonNode, String legalRegistrationNumber) {
         // Get the legal registration number from the credentialSubject field
         JsonNode legalRegistrationNumberNode = jsonNode
-            .get("credentialSubject")
-            .get("legalRegistrationNumber");
+                .get("credentialSubject")
+                .get("legalRegistrationNumber");
 
-            log.info("legalRegistrationNumber  : " + legalRegistrationNumberNode.asText());
-            
-            if (legalRegistrationNumberNode.asText().equals(legalRegistrationNumber)){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
+        //log.info("received legalRegistrationNumber  : " + legalRegistrationNumberNode.asText());
 
-        private boolean isRole(JsonNode jsonNode, String role) {
-            // get to the legalRegistrationNumber field
-    
-            JsonNode roleNumberNode = jsonNode
-            .get("credentialSubject")
-            .get("role");
-    
-        log.info("role  : " + roleNumberNode.asText());
-        
+        if (!legalRegistrationNumberNode.asText().equals(legalRegistrationNumber)) {
+            return false;
+        } 
+        return true;        
+    }
+
+    private boolean isRole(JsonNode jsonNode, String role) {
+        // get to the legalRegistrationNumber field
+
+        JsonNode roleNumberNode = jsonNode
+                .get("credentialSubject")
+                .get("role");
+
+        //log.info("received role  : " + roleNumberNode.asText());
+
         // Check if the role matches
-        if (roleNumberNode.asText().equals(role)){
-            return true;
-        }
-        else{
+        if (!roleNumberNode.asText().equals(role)) {
             return false;
         }
+        return false;
     }
 
 }
-
